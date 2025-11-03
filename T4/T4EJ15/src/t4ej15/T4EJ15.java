3c90d461c6e8bc4704f47599f8602db65bdb224a
vjp-manuelDL <mdelgadol10@informatica.iesvalledeljerteplasencia.es>
2025-11-03 12:22:00 +0100

    17

--- T4/T4EJ15/src/t4ej15/T4EJ15.java
+++ T4/T4EJ15/src/t4ej15/T4EJ15.java
@@ -2,14 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package t3ej32;
+package t4ej15;
 import java.util.Scanner;
-
 /**
  *
  * @author Admin
  */
-public class T3EJ32 {
+public class T4EJ15 {
+    
+    public static int elijoSuma(){
+        int num1;
+        int num2;
+        int suma = num1 + num2;
+        System.out.println("El resultado de la suma es: " + suma);
+        return suma;
+    }
 
     /**
      * @param args the command line arguments
--- T4/T4EJ17/src/t4ej17/T4EJ17.java
+++ T4/T4EJ17/src/t4ej17/T4EJ17.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package t4ej17;
+
 import java.util.Scanner;
 
 /**
@@ -10,21 +11,44 @@ import java.util.Scanner;
  * @author alumno
  */
 public class T4EJ17 {
-    public static double letraRandom(){
-        double codigoASCII = Math.floor((Math.random())*122 + 97);
-        return  codigoASCII;
+    //preparo el random con code ASCII para el abecedario
+    public static char letraRandom() {
+        int codigoASCII = (int) Math.floor((Math.random()) * 26 + 97);
+        return (char) codigoASCII;
     }
-    
-    public static char letraIntro(){
+    //Aqui es para hacer que usuario introduzca las letras
+    public static char letraIntro() {
         Scanner entrada = new Scanner(System.in);
         char letra = entrada.nextLine().charAt(0);
+        return letra;
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        char letraSecreta = letraRandom();
+        char intento;
+        int intentos = 0;
+        //defino las variables y empiezo el bucle
+        System.out.println("¡Adivina la letra del abecedario!");
+
+        do {
+            intento = letraIntro();
+            intentos++;
+            // el bucle hasta que intento = la letra random generada
+            if (intento < letraSecreta) {
+                System.out.println("La letra esta DESPUES en el alfabeto");
+            }
+            if (intento > letraSecreta) {
+                System.out.println("La letra esta ANTES en el alfabeto");
+            }
+
+        } while (intento != letraSecreta);
+        System.out.println("¡¡ENHORABUENA ACERTASTE!! la letra era: " 
+                + letraSecreta);
+        System.out.println("El numero de intentos fue: " + intentos);
         // TODO code application logic here
     }
-    
+
 }
